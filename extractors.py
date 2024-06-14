59691c97c9bcc85009329a02f80f97c256cbcae5
jrhoads <jrhoads@datacite.org>
2024-06-14 05:18:59 -0400

    Tighten up extractors.  Return none if passed falsy value

--- extractors.py
+++ extractors.py
@@ -2,8 +2,10 @@ import re
 
 ROR_PREFIX = "https://ror.org/"
 
-
 def extract_orcid(orcid_string):
+    """Extracts ORCID from a string and transforms it into canonical form"""
+    if not orcid_string:
+        return None
     orcid_regex = re.compile(
         r"(?:https?://orcid\.org/)?(\b\d{4}-\d{4}-\d{4}-\d{3}[0-9X]\b)", re.I
     )
@@ -12,6 +14,9 @@ def extract_orcid(orcid_string):
 
 
 def extract_doi(doi_string):
+    """Extracts DOI from a string and transforms it into canonical form"""
+    if not doi_string:
+        return None
     doi_regex = re.compile(
         r"^(?:https?://doi\.org/)?(10\.\d{4,9}/[-._;()/:A-Z0-9]+)$", re.I
     )
@@ -21,6 +26,8 @@ def extract_doi(doi_string):
 
 def extract_ror_id(ror_string):
     """Extracts ROR id from a string and transforms it into canonical form"""
+    if not ror_string:
+        return None
     ror_regex = re.compile(r"^(?:(?:(?:http|https):\/\/)?ror\.org\/)?(0\w{6}\d{2})$")
     matches = ror_regex.match(ror_string)
     return ROR_PREFIX + matches.group(1) if matches else None
