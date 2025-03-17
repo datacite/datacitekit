d19a3e0281509ab54cc8750402c2b5a6283a1c00
jrhoads <jrhoads@datacite.org>
2025-03-17 12:23:10 +0100

    Add connections and graph endpoints

--- examples/api.py
+++ examples/api.py
@@ -1,4 +1,6 @@
+import itertools
 import os
+from collections import defaultdict
 
 from datacitekit.doi_relations import DoiRelationRelatonsReport
 from datacitekit.extractors import extract_doi
@@ -11,7 +13,6 @@ app = Flask(__name__)
 
 
 @app.route("/doi/related-graph/<path:doi>", methods=["GET"])
-def related_works(doi):
 def related_graph(doi):
     doi = extract_doi(doi)
     if not doi:
@@ -24,6 +25,22 @@ def related_graph(doi):
         return jsonify({"error": "DOI not found"}), 404
 
     report = RelatedWorkReports(full_doi_attributes)
+    return jsonify(
+        {
+            "nodes": report.aggregate_counts,
+            "edges": report.type_connection_report,
+        }
+    )
+
+
+def transpose_defaultdict(my_dict):
+    transposed = defaultdict(list)
+    for key, values in my_dict.items():
+        for value in values:
+            transposed[value].append(key)
+    return dict(transposed)
+
+
 @app.route("/doi/connections/<path:doi>", methods=["GET"])
 def connections(doi):
     doi = extract_doi(doi)
@@ -41,12 +58,13 @@ def connections(doi):
     relation_counts = dict(
         [(relation, len(values)) for relation, values in relations.items() if values]
     )
+    all_relations = transpose_defaultdict(relations)
 
     return jsonify(
         {
-            "raw": full_doi_attributes,
-            "relations": report.relations_to_doi(doi),
+            "relations": relations,
             "counts": relation_counts,
+            "all_relations:": all_relations,
         }
     )
 
